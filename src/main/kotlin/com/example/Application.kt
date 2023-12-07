9b5af4b740a3cc9a24e20852565df6f4703afd41
vladi <vladislavturin333@gmail.com>
2023-12-08 01:29:51 +0300

    project

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,26 @@
+package com.example
+
+import com.example.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.routing.*
+
+
+fun main() {
+    embeddedServer(
+        Netty,
+        watchPaths = listOf("com.example"),
+        port = 8080, host = "0.0.0.0",
+        module = Application::module)
+        .start(wait = true)
+}
+
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureSecurity()
+    configureRouting()
+}
